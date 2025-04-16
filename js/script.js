eeb3a92969b2b41de4653c5ebe7797d00177384e
Vee339 <81091817+Vee339@users.noreply.github.com>
2025-04-16 09:41:43 -0400

    Add the fragments

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to handle play and pause buttons on the Home page
-
-document.getElementById("playButton").addEventListener("click", function() {
-    document.getElementById("audioPlayer").play();
-});
-
-document.getElementById("pauseButton").addEventListener("click", function() {
-    document.getElementById("audioPlayer").pause();
-});
